b1efd837916d3805cba0d3fa35de3a9038caca8c
jpbetanza <jpbetanza99@gmail.com>
2023-08-23 18:05:59 -0300

    some small testes

--- src/components/Botao/index.jsx
+++ src/components/Botao/index.jsx
@@ -5,11 +5,11 @@ import {ReactComponent as ArrowL} from '../../assets/left_arrow.svg'
 const Botao=({position,onPress,page})=>{
     if(position=='left'){
         if(page==0){
-            return(<div className='w-16 h-screen'></div>)
+            return(<div className='w-8 md:w-16 h-screen'></div>)
         }
         else{
             return(
-                <div onClick={onPress} className='w-16 h-screen'>
+                <div onClick={onPress} className='w-8 md:w-16 h-screen'>
                     <ArrowL className='text-white opacity-20 hover:cursor-pointer hover:opacity-40 duration-200 w-full h-full'/>
                 </div>
             )
@@ -17,11 +17,11 @@ const Botao=({position,onPress,page})=>{
     }
     else if(position=='right'){
         if(page==3){
-            return(<div className='w-16 h-screen'></div>)
+            return(<div className='w-8 md:w-16 h-screen'></div>)
         }
         else{
             return(
-                <div onClick={onPress} className='w-16 h-screen'>
+                <div onClick={onPress} className='w-8 md:w-16 h-screen'>
                     <ArrowR  className='text-white opacity-20 hover:cursor-pointer hover:opacity-40 duration-200 w-full h-full'/>
                 </div>
             )
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -11,8 +11,8 @@ const Projects = () => {
     })
     return(
         <div className={`${init ? 'opacity-100':'opacity-0'} duration-1000 flex flex-col items-center `}>
-            <h1 className="sm:text-6xl text-4xl text-white mb-14 drop-shadow-2xl ">Meus Projetos</h1>
-            <div className="w-3/4 p-5 grid grid-rows-1 grid-cols-2 gap-10 md:gap-1 xl:gap-10 md:grid-cols-3">
+            <h1 className="sm:text-6xl text-4xl text-white md:mb-14 mb-5 drop-shadow-2xl ">Meus Projetos</h1>
+            <div className="w-full grid grid-rows-1 grid-cols-2 md:grid-cols-3">
                 {db.map((x) => (
                     <ProjectCard titulo={x.nome} desc={x.desc} url={x.url} src={x.src} imgautor={x.imgautor} utils={x.utils} />
                 ))}
