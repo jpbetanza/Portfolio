58f19f6878066c34adbfdef216c0e3f25a2722ea
jpbetanza <jpbetanza99@gmail.com>
2023-08-25 12:54:26 -0300

    adaptada altura do container principal e ajuste de fonte para celulares muito pequenos

--- src/components/ToolsUsed/index.jsx
+++ src/components/ToolsUsed/index.jsx
@@ -5,7 +5,7 @@ import { tools } from "../Projects/DB"
 const ToolsUsed = ()=>{
 
     return(
-        <div className="bg-opacity-20 bg-black rounded-xl w-3/4 md:w-1/2 xl:w-1/3 shadow-2xl duration-200">
+        <div className="bg-opacity-20 bg-black rounded-xl sm:w-3/4 md:w-1/2 xl:w-1/3 shadow-2xl duration-200">
             <h1 className="text-white md:text-2xl text-xl text-center md:mt-5 mt-3">Ferramentas que utilizo</h1>
             <div className="items-center  grid grid-rows-2 grid-cols-3 md:gap-5 md:p-5 gap-1 p-1">
                 {tools.map((x) => <ToolCard name={x.nome} logo={x.img} />)}
