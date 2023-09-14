9a68195335f410808299f60556829d2d202c46de
jpbetanza <jpbetanza99@gmail.com>
2023-09-14 18:29:51 -0300

    mudanças na introdução

--- src/components/IntroductionText/index.jsx
+++ src/components/IntroductionText/index.jsx
@@ -3,7 +3,10 @@ const IntroductionText = () => {
         <div className="sm:p-5 flex flex-col gap-4">
             <h1 className="text-white text-3xl sm:text-5xl select-none"> Sou desenvolvedor <b>FullStack</b> </h1>
             <p className="text-white select-none">Formado em Engenharia Biomédica, me apaixonei pela área da programação ainda na graduação,
-            onde diversos dos meus projetos de curso foram feitos com foco em desenvolvimento web.</p>
+            onde diversos dos meus projetos de curso foram feitos com foco em desenvolvimento web. Gosto de criar aplicações responsivas e agradáveis 
+            ao olhar. Apesar de me envolver mais com o Front-End, utilizando conhecimentos em React, Tailwind e Bootstrap, eu também tenho familiaridade 
+            com o Back-End, com o uso do Django e NodeJS.
+            </p>
         </div>
     )
 }
