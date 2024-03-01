a14361bfcb71db03e71dbf212f58e87cfe6707c5
jpbetanza <jpbetanza99@gmail.com>
2024-03-01 11:18:41 -0300

    font config on 'tools used' table

--- src/components/ToolCard/index.jsx
+++ src/components/ToolCard/index.jsx
@@ -2,7 +2,7 @@ const ToolCard = ({logo, name})=>{
     return(
         <div className='w-20 h-20 md:w-28 md:h-28 xl:w-28 xl:h-28 flex flex-col justify-center place-self-center  items-center p-5 hover:p-4 md:hover:p-3 duration-200 text-lg'>
             <img className='aspect-square drop-shadow-2xl' src={logo}></img>
-            <h1 className='select-none text-white text-center'>{name}</h1>
+            <h1 className='select-none text-white text-center font-light sm:font-normal'>{name}</h1>
         </div>
     )
 }
