e25f55940692c900dbc7e48ce613eb2225d42d0a
jpbetanza <jpbetanza99@gmail.com>
2024-03-07 14:01:40 -0300

    navbar margin top update

--- src/components/TabNav/index.jsx
+++ src/components/TabNav/index.jsx
@@ -5,7 +5,7 @@ const LangButton = ({page,pageChange}) => {
     const { i18n } = useTranslation();
 
     return(
-        <div className="select-none absolute flex gap-1 md:mt-5 mt-2 right-3 md:right-auto md:left-1/2 md:transform md:-translate-x-1/2 sm:gap-3 ">
+        <div className="select-none absolute flex gap-1 md:mt-5 mt-3 right-3 md:right-auto md:left-1/2 md:transform md:-translate-x-1/2 sm:gap-3 ">
             {i18n.language=='pt'? 
                 navBarText.map((x) => (
                     <button onClick={()=>pageChange(x.page)} className={`${page==x.page?'bg-black bg-opacity-50':'bg-white'}  bg-opacity-10  flex rounded-xl hover:bg-black hover:bg-opacity-40 px-2 sm:px-5 py-1 duration-300`}>
