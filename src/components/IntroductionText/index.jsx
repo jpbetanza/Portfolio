553226e13525bddb25f20843723f7c007af2a28c
jpbetanza <jpbetanza99@gmail.com>
2024-07-05 12:19:38 -0300

    updated profile text size

--- src/components/IntroductionText/index.jsx
+++ src/components/IntroductionText/index.jsx
@@ -3,10 +3,10 @@ import { useTranslation } from "react-i18next";
 const IntroductionText = () => {
     const {t} = useTranslation()
     return(
-        <div className="sm:p-5 flex flex-col gap-4">
-            <h1 className="text-white text-3xl sm:text-5xl select-none"> {t('introductionTitle')} </h1>
-            <p className="text-white select-none"> {t('introductionText1')} </p>
-            <p className="text-white select-none"> {t('introductionText2')} </p>
+        <div className="sm:p-2 flex flex-col gap-2">
+            <h1 className="text-white text-3xl xl:text-5xl select-none"> {t('introductionTitle')} </h1>
+            <p className="text-white text-sm select-none"> {t('introductionText1')} </p>
+            <p className="text-white text-sm select-none"> {t('introductionText2')} </p>
         </div>
     )
 }
